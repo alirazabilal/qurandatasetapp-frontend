c44e01170bc56b74aa67c044686a1ce0dab96efc
alirazabilal <129405525+alirazabilal@users.noreply.github.com>
2025-09-03 17:12:30 +0500

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './App.css';
 
 function Login() {
@@ -74,5 +74,6 @@ function Login() {
 }
 
 export default Login;
+
 
 
